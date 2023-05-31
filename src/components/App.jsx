14be6417fbe50e9b3ee4f410527c0589b25002f9
Ruslan379 <arnov@i.ua>
2023-06-01 02:53:52 +0300

    Add Notification-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { getAllMarkets } from 'redux/market/marketOperations'; //!!!
 
