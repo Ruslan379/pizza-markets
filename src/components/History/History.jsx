708adc015abd020ad1d685e5056dd46aed07a64f
Ruslan379 <arnov@i.ua>
2023-06-01 06:54:43 +0300

    Fixed the logic of all pages

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -5,18 +5,19 @@ import css from './History.module.css';
 
 export const History = () => {
 
-    let customerDataLocalStorage = JSON.parse(localStorage.getItem("customerData"));
+    const customerDataLocalStorage = JSON.parse(localStorage.getItem("customerData"));
 
-    if (!customerDataLocalStorage) {
-        const customerData = {
-            name: "",
-            email: "",
-            phone: "",
-            address: "",
-        };
-        localStorage.setItem("customerData", JSON.stringify(customerData));
-        customerDataLocalStorage = JSON.parse(localStorage.getItem("customerData"));
-    };
+
+    // if (!customerDataLocalStorage) {
+    //     const customerData = {
+    //         name: "",
+    //         email: "",
+    //         phone: "",
+    //         address: "",
+    //     };
+    //     localStorage.setItem("customerData", JSON.stringify(customerData));
+    //     customerDataLocalStorage = JSON.parse(localStorage.getItem("customerData"));
+    // };
 
 
     const allChoicePizzasLocalStorage = JSON.parse(localStorage.getItem("allChoicePizzas"));
@@ -41,24 +42,59 @@ export const History = () => {
     return (
         <div className={css.orderHistoryContainer}>
             <div className={css.historyData}>
-                <p className={css.header}>You Data:</p>
-                {/* <br /> */}
-                <p className={css.textKey}>You Name: <span className={css.textValue}>{customerDataLocalStorage.name}</span></p>
-                <p className={css.textKey}>You Email: <span className={css.textValue}>{customerDataLocalStorage.email}</span></p>
-                <p className={css.textKey}>You Phone: <span className={css.textValue}>{customerDataLocalStorage.phone}</span></p>
-                <p className={css.textKey}>You Address: <span className={css.textValue}>{customerDataLocalStorage.address}</span></p>
+                {customerDataLocalStorage.name
+                    ?
+                    (
+                        <>
+                            <p className={css.header}>You Data:</p>
+                            {/* <br /> */}
+                            <p className={css.textKey}>You Name: <span className={css.textValue}>{customerDataLocalStorage.name}</span></p>
+                            <p className={css.textKey}>You Email: <span className={css.textValue}>{customerDataLocalStorage.email}</span></p>
+                            <p className={css.textKey}>You Phone: <span className={css.textValue}>{customerDataLocalStorage.phone}</span></p>
+                            <p className={css.textKey}>You Address: <span className={css.textValue}>{customerDataLocalStorage.address}</span></p>
+                        </>
+                    )
+                    :
+                    (
+
+                        <div className={css.informationTextContainer}>
+                            <div className={css.informationText}
+                            >
+                                Please enter your information
+                            </div>
+                        </div>
+                    )
+                }
             </div>
-            
             <div className={css.historyOrder}>
-                <p className={css.header}>You Order:</p>
-                {/* <br /> */}
-                <p className={css.textKey}>Pizza varieties: <span className={css.textValue}>{allChoicePizzasLocalStorage ? allChoicePizzasLocalStorage.length : 0}</span></p>
-                <p className={css.textKey}>Total pizzas: <span className={css.textValue}>{totalPizzas}</span></p>
-                <p className={css.textKey}>TOTAL: <span className={css.textValue}>{totalPrice} грн.</span></p>
+                {allChoicePizzasLocalStorage
+                    ?
+                    (
+                        <>
+                            <p className={css.header}>You Order:</p>
+                            {/* <br /> */}
+                            <p className={css.textKey}>Pizza varieties: <span className={css.textValue}>{allChoicePizzasLocalStorage ? allChoicePizzasLocalStorage.length : 0}</span></p>
+                            <p className={css.textKey}>Total pizzas: <span className={css.textValue}>{totalPizzas}</span></p>
+                            <p className={css.textKey}>TOTAL: <span className={css.textValue}>{totalPrice} грн.</span></p>
+                        </>
+                    )
+                    :
+                    (
+
+                        <div className={css.informationTextContainer}>
+                            <div className={css.informationText}
+                            >
+                                Please, place your pizza order on the shop page make an order
+                            </div>
+                        </div>
+                    )
+                }
             </div>
             <div className={css.buttonContainer}>
                 <NavLink className={css.linkButton} to="/cart">Change</NavLink>
-                <NavLink className={css.linkButton} to="/order">Сonfirm</NavLink>
+                {customerDataLocalStorage.name && allChoicePizzasLocalStorage && (
+                    <NavLink className={css.linkButton} to="/order">Сonfirm</NavLink>
+                )}
             </div>
         </div>
     );
