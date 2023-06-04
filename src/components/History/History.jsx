a4cf05ac5419b97d9ef1411779042aca3796fa5f
Ruslan379 <arnov@i.ua>
2023-06-04 18:11:34 +0300

    Loader added to the History Page

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,8 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux'; //!!!
+import { useDispatch, useSelector } from 'react-redux'; //!!!
 
 import { addOrder } from 'redux/orders/ordersOperations';
+import { selectLoading } from 'redux/orders/ordersSelectors';
+
+import { Loader } from 'components/Loader/Loader.jsx';
 
 import css from './History.module.css';
 
@@ -12,6 +15,7 @@ import css from './History.module.css';
 export const History = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const isLoading = useSelector(selectLoading);
 
     const customerDataLocalStorage = JSON.parse(localStorage.getItem("customerData"));
     const allChoicePizzasLocalStorage = JSON.parse(localStorage.getItem("allChoicePizzas"));
@@ -50,68 +54,79 @@ export const History = () => {
 
     return (
         <div className={css.orderHistoryContainer}>
-            <div className={css.historyData}>
-                {(customerDataLocalStorage && customerDataLocalStorage.name)
-                    ?
-                    (
-                        <>
-                            <p className={css.header}>You Data:</p>
-                            {/* <br /> */}
-                            <p className={css.textKey}>You Name: <span className={css.textValue}>{customerDataLocalStorage.name}</span></p>
-                            <p className={css.textKey}>You Email: <span className={css.textValue}>{customerDataLocalStorage.email}</span></p>
-                            <p className={css.textKey}>You Phone: <span className={css.textValue}>{customerDataLocalStorage.phone}</span></p>
-                            <p className={css.textKey}>You Address: <span className={css.textValue}>{customerDataLocalStorage.address}</span></p>
-                        </>
-                    )
-                    :
-                    (
+            {isLoading
+                ?
+                <Loader
+                    textOne={"Please wait..."}
+                    textTwo={"Your order is being processed."}
+                />
+                :
+                <>
+                    <div className={css.historyData}>
+                        {(customerDataLocalStorage && customerDataLocalStorage.name)
+                            ?
+                            (
+                                <>
+                                    <p className={css.header}>You Data:</p>
+                                    {/* <br /> */}
+                                    <p className={css.textKey}>You Name: <span className={css.textValue}>{customerDataLocalStorage.name}</span></p>
+                                    <p className={css.textKey}>You Email: <span className={css.textValue}>{customerDataLocalStorage.email}</span></p>
+                                    <p className={css.textKey}>You Phone: <span className={css.textValue}>{customerDataLocalStorage.phone}</span></p>
+                                    <p className={css.textKey}>You Address: <span className={css.textValue}>{customerDataLocalStorage.address}</span></p>
+                                </>
+                            )
+                            :
+                            (
 
-                        <div className={css.informationTextContainer}>
-                            <div className={css.informationText}
+                                <div className={css.informationTextContainer}>
+                                    <div className={css.informationText}
+                                    >
+                                        Please enter your information
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className={css.historyOrder}>
+                        {allChoicePizzasLocalStorage && allChoicePizzasLocalStorage.length > 0
+                            ?
+                            (
+                                <>
+                                    <p className={css.header}>You Order:</p>
+                                    {/* <br /> */}
+                                    <p className={css.textKey}>Pizza varieties: <span className={css.textValue}>{allChoicePizzasLocalStorage ? allChoicePizzasLocalStorage.length : 0}</span></p>
+                                    <p className={css.textKey}>Total pizzas: <span className={css.textValue}>{totalPizzas}</span></p>
+                                    <p className={css.textKey}>TOTAL: <span className={css.textValue}>{totalPrice} грн.</span></p>
+                                </>
+                            )
+                            :
+                            (
+                            
+                                <div className={css.informationTextContainer}>
+                                    <div className={css.informationText}
+                                    >
+                                        Please, place your pizza order on the shop page make an order
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className={css.buttonContainer}>
+                        <NavLink className={css.linkButton} to="/cart">Change</NavLink>
+                        {customerDataLocalStorage && customerDataLocalStorage.name && allChoicePizzasLocalStorage && (
+                            <button
+                                className={`${css.linkButton} ${css.linkButtonConfirm}`}
+                                type="button"
+                                onClick={() => completionOfTheOrder(totalPrice)}
                             >
-                                Please enter your information
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
-            <div className={css.historyOrder}>
-                {allChoicePizzasLocalStorage && allChoicePizzasLocalStorage.length > 0
-                    ?
-                    (
-                        <>
-                            <p className={css.header}>You Order:</p>
-                            {/* <br /> */}
-                            <p className={css.textKey}>Pizza varieties: <span className={css.textValue}>{allChoicePizzasLocalStorage ? allChoicePizzasLocalStorage.length : 0}</span></p>
-                            <p className={css.textKey}>Total pizzas: <span className={css.textValue}>{totalPizzas}</span></p>
-                            <p className={css.textKey}>TOTAL: <span className={css.textValue}>{totalPrice} грн.</span></p>
-                        </>
-                    )
-                    :
-                    (
-
-                        <div className={css.informationTextContainer}>
-                            <div className={css.informationText}
-                            >
-                                Please, place your pizza order on the shop page make an order
-                            </div>
-                        </div>
-                    )
-                }
-            </div>
-            <div className={css.buttonContainer}>
-                <NavLink className={css.linkButton} to="/cart">Change</NavLink>
-                {customerDataLocalStorage && customerDataLocalStorage.name && allChoicePizzasLocalStorage && (
-                    // <NavLink className={css.linkButton} to="/order">Сonfirm</NavLink>
-                    <button
-                        className={`${css.linkButton} ${css.linkButtonConfirm}`}
-                        type="button"
-                        onClick={() => completionOfTheOrder(totalPrice)}
-                    >
-                        Сonfirm
-                    </button>
-                )}
-            </div>
+                                Сonfirm
+                            </button>
+                        )}
+                    </div>
+                </>
+            }
         </div>
+        
+        
     );
 };
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Audio } from  'react-loader-spinner'
 
 
-export const Loader = () => (
-    <h1 style={{ margin: '5px auto' }}>
+export const Loader = ({ textOne, textTwo }) => (
+    <h1 style={{ margin: '5px auto', color: '#154407' }}>
         {/* Please wait... */}
+        {textOne}
+        < br />
+        {textTwo}
         <Audio
             width="100%"
         />
