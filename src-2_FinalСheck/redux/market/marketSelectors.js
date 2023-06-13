efebf9a342d83d19b892f8d77d7304ea6174d975
Ruslan379 <arnov@i.ua>
2023-06-13 16:30:14 +0300

    Removing unnecessary files - FINAL

--- src-2_FinalСheck/redux/market/marketSelectors.js
+++
@@ -1,4 +0,0 @@
-export const selectLoadingMarkets = state => state.marketPizzas.isLoading;
-
-export const selectAllMarkets = state => state.marketPizzas.allMarkets;
-
--- src-2_FinalСheck/redux/orders/ordersSelectors.js
+++
@@ -1,7 +0,0 @@
-
-export const selectLoading = state => state.customerOrder.isLoading;
-
-export const selectAllOrders = state => state.customerOrder.orders;
-
-export const selectLastOrderNumber = state => state.customerOrder.lastOrderNumber;
-
